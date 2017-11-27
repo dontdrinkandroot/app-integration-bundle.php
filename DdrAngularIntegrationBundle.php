1b9aa23e88499dc712babc5302889c3eb41c968b
Philip Washington Sorst <philip@sorst.net>
2017-11-27 14:50:49 +0100

    Adding Bundle and namespace

---
+++ DdrAngularIntegrationBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dontdrinkandroot\AngularIntegrationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class DdrAngularIntegrationBundle extends Bundle
+{
+
+}
