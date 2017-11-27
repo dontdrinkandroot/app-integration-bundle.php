f4f94d909b8440505cba6b0621d7547e4c3435f5
Philip Washington Sorst <philip@sorst.net>
2017-11-27 15:06:32 +0100

    Adding symfony/console

---
+++ Command/AngularPrepareCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class AngularPrepareCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('ddr:angular:prepare');
+    }
+}
